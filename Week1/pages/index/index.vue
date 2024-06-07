c762144be71fa810c41cf2838cefb449c927006a
Baileylin <baileylinhfi@gmail.com>
2024-06-06 21:11:22 -0400

    update

---
+++ Week1/pages/index/index.vue
@@ -0,0 +1,58 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<div v-html="data"></div>
+	</view>
+</template>
+
+<script>
+	import { ref, onMounted } from 'vue';
+	export default {
+		setup() {
+		    const data = ref(null); // Reactive property to store API result
+		
+		    const fetchData = ()=>{uni.request({
+			    url: 'http://localhost:8080/home',
+			    success: (res) => {
+			        data.value = res.data
+			    }
+			});}
+		
+		    onMounted(() => {
+		      fetchData(); // Make API call when the component is mounted
+		    });
+		
+		    return {
+		      data
+		    };
+		  }
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
---
+++ Week1/servers/routes.js
@@ -0,0 +1,10 @@
+// Route 1: GET /author/:type
+const sample = async function(req, res) {
+	res.send("Hello World!")
+}
+
+
+
+module.exports = {
+	sample
+}
